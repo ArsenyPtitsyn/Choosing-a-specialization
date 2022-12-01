5fd60a01618f8a23be4e3ff0843e8b7c3af3641f
Arseny Ptitsyn <senyabird@yandex.ru>
2022-12-02 00:43:41 +0300

    The program is written

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] getStringsEqualOrLessThenNSimbols(string[] stringArray, int maxNumberOfSimbols)
+{
+    string resultString = string.Empty;
+    string separator = ",";
+
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        if (stringArray[i].Length <= maxNumberOfSimbols)
+        {
+            resultString += stringArray[i] + separator;
+        }
+    }
+
+    if (resultString.Length > 0)
+    {
+        return resultString.Substring(0, resultString.Length - 1).Split(separator);
+    }
+    else
+    {
+        return resultString.Split();
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+string[] array = { "false", "true" };
+PrintArray(array);
+string[] resultArray = getStringsEqualOrLessThenNSimbols(array, 3);
+PrintArray(resultArray);
